c371233436a0e8212d361792d638f44a5f3d0322
darien786 <darienbaltazar2@gmail.com>
2023-12-26 19:19:53 -0600

    Login

--- app/src/main/java/mx/uv/smartcupon/MainActivity.kt
+++ app/src/main/java/mx/uv/smartcupon/MainActivity.kt
@@ -4,12 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
 import android.widget.Toast
 import com.google.gson.Gson
 import com.koushikdutta.ion.Ion
 import mx.uv.smartcupon.databinding.ActivityMainBinding
-import mx.uv.smartcupon.modelo.AutenticacionDAO
 import mx.uv.smartcupon.modelo.poko.Cliente
 import mx.uv.smartcupon.modelo.poko.RespuestaLogin
 import mx.uv.smartcupon.modelo.util.Constantes
@@ -30,14 +28,29 @@ class MainActivity : AppCompatActivity() {
             var email = bindig.edtCorreo.text.toString()
             var password = bindig.etpPassword.text.toString()
 
-            cliente.email = email.toString()
-            cliente.password = password.toString()
 
-            Toast.makeText(this@MainActivity, cliente.email + cliente.password, Toast.LENGTH_LONG).show()
 
-            realizarPeticionLogin(cliente)
+            if(!validarCamposLogin(email, password)){
+                cliente.email = email.toString()
+                cliente.password = password.toString()
+
+                realizarPeticionLogin(cliente)
+            }
 
         }
+    }
+
+    fun validarCamposLogin(email: String, password: String) : Boolean {
+        if (email.isEmpty()){
+            bindig.edtCorreo.error = "Correo electrónico obligatorio"
+            return true
+        }
+        if(password.isEmpty()){
+            bindig.etpPassword.error = "Contraseña obligatoria"
+            return true
+        }
+
+        return false
     }
 
 
@@ -56,7 +69,6 @@ class MainActivity : AppCompatActivity() {
             .asString()
             .setCallback { e, result ->
                 if (e==null && result != null){
-                    Toast.makeText(this@MainActivity, "Respuesta del servidor: $result", Toast.LENGTH_SHORT).show()
                     serializarRespuestaLogin(result.toString())
                 }else{
                     Toast.makeText(this@MainActivity, "Error en la peticion", Toast.LENGTH_SHORT).show()
@@ -67,15 +79,21 @@ class MainActivity : AppCompatActivity() {
     fun serializarRespuestaLogin(json: String ){
         val gson = Gson()
         var respuesta: RespuestaLogin = gson.fromJson(json,RespuestaLogin::class.java)
+        Toast.makeText(this@MainActivity, respuesta.mensaje, Toast.LENGTH_LONG).show()
 
         if(!respuesta.error){
             irPantallaHome(respuesta.cliente)
         }
     }
 
-    fun irPantallaHome(cliente: Cliente){
+    fun irPantallaHome(sesionCliente: Cliente){
+
+        val gson = Gson()
+        var cadenaJson:String = gson.toJson(sesionCliente)
 
         val intent = Intent(this@MainActivity, HomeActivity::class.java)
+        intent.putExtra("cliente", cadenaJson)
+
         startActivity(intent)
         finish()
     }
--- app/src/main/java/mx/uv/smartcupon/RegistroClienteActivity.kt
+++ app/src/main/java/mx/uv/smartcupon/RegistroClienteActivity.kt
@@ -4,8 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.google.gson.Gson
+import com.koushikdutta.ion.Ion
 import mx.uv.smartcupon.databinding.ActivityMainBinding
 import mx.uv.smartcupon.databinding.ActivityRegistroClienteBinding
+import mx.uv.smartcupon.modelo.poko.Cliente
+import mx.uv.smartcupon.modelo.poko.DatosCliente
+import mx.uv.smartcupon.modelo.poko.Direccion
+import mx.uv.smartcupon.modelo.poko.Mensaje
+import mx.uv.smartcupon.modelo.poko.RespuestaLogin
+import mx.uv.smartcupon.modelo.util.Constantes
 
 class RegistroClienteActivity : AppCompatActivity() {
 
@@ -18,13 +27,108 @@ class RegistroClienteActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        var cliente = Cliente()
+        var direccion = Direccion()
+        var datosCliente = DatosCliente()
+
+        cliente.nombre = binding.edtNombre.text.toString()
+        cliente.apellidoPaterno = binding.edtApellidoP.text.toString()
+        cliente.apellidoMaterno = binding.edtApellidoM.text.toString()
+        cliente.telefono = binding.edtNumTelefono.text.toString()
+        cliente.email = binding.edtEmail.text.toString()
+        cliente.fechaNacimiento = binding.edtFechaNacimiento.text.toString()
+        cliente.password = binding.edtPassword.text.toString()
+        direccion.calle = binding.edtCalle.text.toString()
+        direccion.numero = binding.edtNumero.text.toString().toInt()
+
+
+
         binding.btnRegistrar.setOnClickListener {
+            if(!validarCamposRegistro(cliente.nombre!!, cliente.apellidoPaterno!!, cliente.apellidoMaterno!!, cliente.telefono!!, cliente.fechaNacimiento!!, cliente.email!!,
+                cliente.password!!,direccion.calle!!, direccion.numero!!)){
+                 datosCliente.cliente = cliente
+                 datosCliente.direccion = direccion
+
+                Toast.makeText(this@RegistroClienteActivity, direccion.numero.toString(), Toast.LENGTH_LONG).show()
+            }
+
             val intent = Intent(this@RegistroClienteActivity,HomeActivity::class.java)
             startActivity(intent)
         }
     }
 
-    fun btnRegistrar(view : View) : Unit{
+    fun validarCamposRegistro(nombre:String, apellidoPaterno:String, apellidoMaterno:String,
+                              telefono:String, fechaNacimiento:String, email:String, password:String,
+                              calle:String, numero:Int): Boolean{
+        if (nombre.isEmpty()){
+            binding.edtNombre.error = "Campo obligatorio"
+            return true
+        }
+        if(apellidoPaterno.isEmpty()){
+            binding.edtApellidoP.error = "Campo obligatorio"
+            return true
+        }
+        if(apellidoMaterno.isEmpty()){
+            binding.edtApellidoM.error = "Campo obligatorio"
+            return true
+        }
+        if(telefono.isEmpty()){
+            binding.edtNumTelefono.error = "Campo obligatorio"
+            return true
+        }
+        if(fechaNacimiento.isEmpty()){
+            binding.edtFechaNacimiento.error = "Campo obligatorio"
+            return true
+        }
+        if(email.isEmpty()){
+            binding.edtEmail.error = "Campo obligatorio"
+            return true
+        }
+        if(password.isEmpty()){
+            binding.edtPassword.error = "Campo obligatorio"
+            return true
+        }
+        if(calle.isEmpty()){
+            binding.edtNumero.error = "Campo obligatorio"
+            return true
+        }
+        if(numero > 0){
+            binding.edtNombre.error = "Campo obligatorio"
+            return true
+        }
+
+        return false
+    }
+
+    fun realizarPeticionRegistro(datosCliente: DatosCliente){
+
+        Ion.with(this@RegistroClienteActivity)
+            .load("POST", "${Constantes.URL_WS}clientes/registrarCliente")
+            .setHeader("Content-Type","application/json")
+            .setJsonPojoBody(datosCliente)
+            .asString()
+            .setCallback { e, result ->
+                if (e==null && result != null){
+                    serializarRespuestaLogin(result)
+                }else{
+                    Toast.makeText(this@RegistroClienteActivity, "Error en la peticion", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    fun serializarRespuestaLogin(json: String) {
+        val gson = Gson()
+        val respuesta = gson.fromJson(json,Mensaje::class.java)
+        if (!respuesta.error){
+            Toast.makeText(this@RegistroClienteActivity, respuesta.mensjae, Toast.LENGTH_SHORT).show()
+            
+        }else{
+            Toast.makeText(this@RegistroClienteActivity, respuesta.mensjae, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun irPantallaHome(cliente: Cliente){
 
     }
+
 }
